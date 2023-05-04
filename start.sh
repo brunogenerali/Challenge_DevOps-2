90d7606945aa033461b05e75a10901755d572f0a
brunogenerali <bruno.generali91@gmail.com>
2023-05-04 17:42:58 -0300

    add nohup to command

--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@ pkill java
 rm -rf api
 git clone https://github.com/brunogenerali/Challenge_DevOps-2.git api && cd api
 mvn clean package -DskipTests
-java -Dspring.profiles.active=prod -DDATASOURCE_URL=jdbc:mysql://localhost/vollmed_api -DDATASOURCE_USERNAME=root -DDATASOURCE_PASSWORD=root -jar target/api-0.0.1-SNAPSHOT.jar &
+nohup java -Dspring.profiles.active=prod -DDATASOURCE_URL=jdbc:mysql://localhost/vollmed_api -DDATASOURCE_USERNAME=root -DDATASOURCE_PASSWORD=root -jar target/api-0.0.1-SNAPSHOT.jar > out.txt 2>&1 &
