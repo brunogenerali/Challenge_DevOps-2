cf128359454f76c6e1e01007d1951d8291725666
brunogenerali <bruno.generali91@gmail.com>
2023-05-09 00:55:17 -0300

    Fix Start Script

--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@ pkill java
 rm -rf api
 git clone https://github.com/brunogenerali/Challenge_DevOps-2.git api && cd api
 mvn clean package -DskipTests
-nohup java -Dspring.profiles.active=prod -DDATASOURCE_URL=jdbc:mysql://localhost/vollmed_api -DDATASOURCE_USERNAME=root -DDATASOURCE_PASSWORD=root -jar target/api-0.0.1-SNAPSHOT.jar > out.txt 2>&1 &
+nohup java -Dspring.profiles.active=prod -DDATASOURCE_URL=jdbc:mysql://54.85.81.159/vollmed_api -DDATASOURCE_USERNAME=root -DDATASOURCE_PASSWORD=root -jar target/api-0.0.1-SNAPSHOT.jar > out.txt 2>&1 &
