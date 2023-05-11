12b93f8a6eb3ef86ed8f017fbfce32bf6ba3f5c3
brunogenerali <bruno.generali91@gmail.com>
2023-05-10 22:18:05 -0300

    CI/CD with tests

--- start.sh
+++ start.sh
@@ -3,5 +3,5 @@
 pkill java
 rm -rf api
 git clone https://github.com/brunogenerali/Challenge_DevOps-2.git api && cd api
-mvn clean package -DskipTests
-nohup java -Dspring.profiles.active=prod -DDATASOURCE_URL=jdbc:mysql://18.212.50.244/vollmed_api -DDATASOURCE_USERNAME=root -DDATASOURCE_PASSWORD=root -jar target/api-0.0.1-SNAPSHOT.jar > out.txt 2>&1 &
+mvn clean package
+nohup java -Dspring.profiles.active=prod -DDATASOURCE_URL=jdbc:mysql://172.31.85.240/vollmed_api -DDATASOURCE_USERNAME=root -DDATASOURCE_PASSWORD=root -jar target/api-0.0.1-SNAPSHOT.jar > out.txt 2>&1 &
