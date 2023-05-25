dffd52843b36f22ba9a33b50a3f4339603f27ac6
brunogenerali <bruno.generali91@gmail.com>
2023-05-24 23:04:40 -0300

    Terraform aws ok Ansible DB to fix

--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@ pkill java
 sudo rm -rf api
 git clone https://github.com/brunogenerali/Challenge_DevOps-2.git api && cd api
 mvn clean package -DskipTests
-nohup java -Dspring.profiles.active=prod -DDATASOURCE_URL=jdbc:mysql://172.31.85.240/vollmed_api -DDATASOURCE_USERNAME=root -DDATASOURCE_PASSWORD=root -jar target/api-0.0.1-SNAPSHOT.jar > out.txt 2>&1 &
+nohup java -Dspring.profiles.active=prod -DDATASOURCE_URL=jdbc:mysql://172.31.85.208/vollmed_api -DDATASOURCE_USERNAME=root -DDATASOURCE_PASSWORD=root -jar target/api-0.0.1-SNAPSHOT.jar > out.txt 2>&1 &
